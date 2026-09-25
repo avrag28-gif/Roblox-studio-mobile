10c506b3fa2bd71043b730d84010a98725d12080
avrag28-gif <avrag28@gmail.com>
2026-09-26 01:01:13 +0700

    engine: add CFrame

--- src/math/cframe.h
+++ src/math/cframe.h
@@ -1,3 +1,3 @@
 #pragma once
 #include "vector3.h"
-namespace rsm { struct CFrame { Vector3 position{}; constexpr CFrame()=default; constexpr explicit CFrame(Vector3 p):position(p){} static CFrame Identity(){return{};} }; }
+namespace rsm { struct CFrame{Vector3 position{};constexpr CFrame()=default;constexpr explicit CFrame(Vector3 p):position(p){}static CFrame Identity(){return{};}}; }
