bb7c3c6f0093d35fea8b9bd737a055027c63d327
avrag28-gif <avrag28@gmail.com>
2026-09-26 01:02:40 +0700

    engine: add top-level engine loop

--- src/engine/engine.h
+++ src/engine/engine.h
@@ -1,4 +1,5 @@
 #pragma once
 #include "../scene/scene.h"
+#include "../renderer/renderer.h"
 #include "../physics/physics_world.h"
-namespace rsm { class Engine { bool running_=false; Scene scene_; PhysicsWorld physics_; public: void Start(){running_=true;} void Stop(){running_=false;} bool Running()const{return running_;} void Tick(float dt){if(running_)physics_.Step(dt);} Scene& EditorScene(){return scene_;} PhysicsWorld& Physics(){return physics_;} }; }
+namespace rsm { class Engine{public:Engine():renderer_(std::make_unique<NullRenderer>()){}bool Initialize(){return renderer_->Initialize();}void Tick(float dt){physics_.Step(dt,scene_.Game());renderer_->Render(scene_.Game());}Scene& EditorScene(){return scene_;}const Renderer& RendererBackend()const{return *renderer_;}private:Scene scene_;PhysicsWorld physics_;std::unique_ptr<Renderer> renderer_;};}
