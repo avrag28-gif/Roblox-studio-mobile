1fff66bb81c3afd221caf33a4d43ab7fef8dae51
avrag28-gif <avrag28@gmail.com>
2026-09-26 01:01:21 +0700

    engine: add scene container

--- src/scene/scene.h
+++ src/scene/scene.h
@@ -1,4 +1,3 @@
 #pragma once
 #include "../core/data_model.h"
-#include <memory>
-namespace rsm { class Scene { std::unique_ptr<DataModel> dataModel_=std::make_unique<DataModel>(); public: DataModel& Data(){return *dataModel_;} const DataModel& Data()const{return *dataModel_;} void Clear(){dataModel_=std::make_unique<DataModel>();} }; }
+namespace rsm { class Scene{public:Scene():game_(std::make_unique<DataModel>()){}DataModel& Game(){return *game_;}const DataModel& Game()const{return *game_;}void Reset(){game_=std::make_unique<DataModel>();}private:std::unique_ptr<DataModel> game_;}; }
