371ccf94e3ddf0827d928baf0ef9d992be4c4303
avrag28-gif <avrag28@gmail.com>
2026-09-26 00:49:15 +0700

    core: strengthen DataModel and Instance behavior

--- src/core/data_model.h
+++ src/core/data_model.h
@@ -1,18 +1,16 @@
 #pragma once
 #include "instance.h"
+#include <memory>
 #include <string>
 #include <unordered_map>
 
 namespace rsm {
-
 class DataModel final : public Instance {
 public:
     DataModel();
-    Instance* GetService(const std::string& serviceName);
-    void InitializeDefaultServices();
-
+    Instance* GetService(const std::string& name);
+    const Instance* GetService(const std::string& name) const;
 private:
     std::unordered_map<std::string, Instance*> services_;
 };
-
 }
