fe4b1e2c7bc0196b9f30a65fc0285776ec24886d
avrag28-gif <avrag28@gmail.com>
2026-09-26 02:09:43 +0700

    Strengthen runtime physics networking and script sandbox

--- src/runtime/runtime.h
+++ src/runtime/runtime.h
@@ -1,4 +1,38 @@
 #pragma once
 #include "../scene/scene.h"
 #include "../physics/physics_world.h"
-namespace rsm {class Runtime{Scene scene_;PhysicsWorld physics_;bool running_=false;public:bool Start(const Scene&source){if(!source.Game().Clone())return false;scene_.Reset();running_=true;return true;}void Step(float dt){if(running_)physics_.Step(dt,scene_.Game());}void Stop(){running_=false;}bool Running()const{return running_;}Scene&Game(){return scene_;}const Scene&Game()const{return scene_;}};}
+#include "../scripting/luau_service.h"
+namespace rsm {
+class Runtime {
+ public:
+  Runtime():script_([this](std::string m){lastLog_=std::move(m);}) {}
+  bool Start(const Scene& source){
+    auto copy=source.Game().Clone();
+    if(!copy)return false;
+    scene_.Reset();
+    auto* dm=dynamic_cast<DataModel*>(copy.release());
+    if(!dm)return false;
+    runtimeRoot_.reset(dm);
+    running_=true;
+    return true;
+  }
+  bool RunScript(const std::string& source){
+    if(!running_)return false;
+    return script_.CompileAndRun(source);
+  }
+  void Step(float dt){if(running_)physics_.Step(dt,scene_.Game());}
+  void Stop(){running_=false;runtimeRoot_.reset();}
+  bool Running()const{return running_;}
+  Scene& Game(){return scene_;}
+  const Scene& Game()const{return scene_;}
+  const std::string& LastLog()const{return lastLog_;}
+  const LuauService& Scripts()const{return script_;}
+ private:
+  Scene scene_;
+  PhysicsWorld physics_;
+  LuauService script_;
+  std::unique_ptr<DataModel> runtimeRoot_;
+  std::string lastLog_;
+  bool running_=false;
+};
+}
