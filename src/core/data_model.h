514df020ed92a6e6507e45a55e102b982646360d
avrag28-gif <avrag28@gmail.com>
2026-09-26 01:52:04 +0700

    feat(core): complete build graph and DataModel cloning

--- src/core/data_model.h
+++ src/core/data_model.h
@@ -1,4 +1,15 @@
 #pragma once
 #include "instance.h"
 #include <unordered_map>
-namespace rsm{class DataModel final:public Instance{public:DataModel();void InitializeDefaultServices();Instance*GetService(const std::string&);const Instance*GetService(const std::string&)const;private:std::unordered_map<std::string,Instance*>services_;};}
+namespace rsm{
+class DataModel final:public Instance{
+public:
+ DataModel();
+ void InitializeDefaultServices();
+ Instance*GetService(const std::string&);
+ const Instance*GetService(const std::string&)const;
+ std::unique_ptr<Instance>Clone()const override;
+private:
+ std::unordered_map<std::string,Instance*>services_;
+};
+}
