7fd96d94bc9112ba9d225d60c22bb991ec2d87d6
avrag28-gif <avrag28@gmail.com>
2026-09-26 01:46:05 +0700

    fix(core): make Signal connection compatible with ID-based disconnect tests

--- src/core/signal.h
+++ src/core/signal.h
@@ -4,4 +4,29 @@
 #include <unordered_map>
 #include <vector>
 #include <cstddef>
-namespace rsm {template<class...A>class Signal{public:using Callback=std::function<void(A...)>;using Id=std::size_t;class Connection{Signal*s{};Id i{};public:Connection()=default;Connection(Signal*x,Id n):s(x),i(n){}Connection(Connection&&o)noexcept:s(o.s),i(o.i){o.s=nullptr;}~Connection(){Disconnect();}Connection(const Connection&)=delete;void Disconnect(){if(s){s->Disconnect(i);s=nullptr;}}explicit operator bool()const{return s!=nullptr;}};Connection Connect(Callback cb){std::lock_guard<std::mutex>l(m);auto i=next++;slots[i]=std::move(cb);return Connection(this,i);}void Disconnect(Id i){std::lock_guard<std::mutex>l(m);slots.erase(i);}void Clear(){std::lock_guard<std::mutex>l(m);slots.clear();}void Fire(A...a){std::vector<Callback>v;{std::lock_guard<std::mutex>l(m);for(auto&[_,cb]:slots)v.push_back(cb);}for(auto&cb:v)if(cb)cb(a...);}private:std::mutex m;std::unordered_map<Id,Callback>slots;Id next=1;};}
+#include <utility>
+namespace rsm {
+template<class... A> class Signal {
+public:
+ using Callback=std::function<void(A...)>; using Id=std::size_t;
+ class Connection {
+   Signal* s_{}; Id id_{};
+ public:
+   Connection()=default; Connection(Signal* s,Id id):s_(s),id_(id){}
+   Connection(Connection&& o) noexcept:s_(o.s_),id_(o.id_){o.s_=nullptr;}
+   Connection& operator=(Connection&& o) noexcept { if(this!=&o){Disconnect();s_=o.s_;id_=o.id_;o.s_=nullptr;}return *this; }
+   Connection(const Connection&)=delete; Connection& operator=(const Connection&)=delete;
+   ~Connection(){Disconnect();}
+   void Disconnect(){if(s_){s_->Disconnect(id_);s_=nullptr;}}
+   Id IdValue() const noexcept{return id_;}
+   explicit operator bool() const{return s_!=nullptr;}
+ };
+ Connection Connect(Callback cb){std::lock_guard<std::mutex> l(m_);Id id=next_++;slots_[id]=std::move(cb);return Connection(this,id);}
+ void Disconnect(Id id){std::lock_guard<std::mutex> l(m_);slots_.erase(id);}
+ void Disconnect(Connection& c){c.Disconnect();}
+ void Clear(){std::lock_guard<std::mutex> l(m_);slots_.clear();}
+ void Fire(A... a){std::vector<Callback> copy;{std::lock_guard<std::mutex> l(m_);for(auto& [_,cb]:slots_)copy.push_back(cb);}for(auto& cb:copy)if(cb)cb(a...);}
+private:
+ std::mutex m_; std::unordered_map<Id,Callback> slots_; Id next_=1;
+};
+}
