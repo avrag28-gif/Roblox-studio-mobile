6f26bea0bf869080b6c393a4deb7b6f4b882d1ea
avrag28-gif <avrag28@gmail.com>
2026-09-26 01:31:32 +0700

    ci: use runner Android SDK manager directly

--- tests/core_instance_test.cpp
+++ tests/core_instance_test.cpp
@@ -1,4 +1,4 @@
-#include "../src/core/data_model.h"
-#include "../src/core/instance_factory.h"
+#include "core/data_model.h"
+#include "core/instance_factory.h"
 #include <cassert>
-int main(){using namespace rsm;DataModel g;auto*w=g.GetService("Workspace");assert(w);bool hit=false;auto connection=w->PropertyChanged.Connect([&](const std::string&n){hit=n=="Name";});w->SetName("World");assert(hit);connection.Disconnect();auto p=InstanceFactory::New("Part");auto*r=Instance::SetParent(std::move(p),w);assert(r&&r->ClassName()=="Part");r->SetName("A");auto c=r->Clone();assert(c&&c->ClassName()=="Part");r->Destroy();assert(!w->FindFirstChild("A"));return 0;}
+int main(){using namespace rsm;DataModel g;auto*w=g.GetService("Workspace");bool hit=false;auto id=w->PropertyChanged.Connect([&](const std::string&n){hit=n=="Name";});w->SetName("World");assert(hit);w->PropertyChanged.Disconnect(id);auto p=InstanceFactory::New("Part");auto*r=Instance::SetParent(std::move(p),w);assert(r&&r->ClassName()=="Part");r->SetName("A");auto c=r->Clone();assert(c&&c->ClassName()=="Part");r->Destroy();assert(!w->FindFirstChild("A"));}
