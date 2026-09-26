3b31d5c89851d5487764306b898994304b9d61f7
avrag28-gif <avrag28@gmail.com>
2026-09-26 07:32:48 +0700

    feat: harden runtime security and profiling interfaces

--- src/runtime/client_server.h
+++ src/runtime/client_server.h
@@ -1,2 +1,15 @@
 #pragma once
-namespace rsm { enum class RuntimeRole{Editor,Server,Client}; class RuntimeContext{RuntimeRole role_;public:explicit RuntimeContext(RuntimeRole r):role_(r){}RuntimeRole Role()const{return role_;}};}
+#include "../network/replication_runtime.h"
+#include "../network/transport.h"
+namespace rsm {
+enum class RuntimeRole{Editor,Server,Client};
+class RuntimeContext {
+ RuntimeRole role_; LoopbackTransport* transport_=nullptr; ReplicationRuntime replication_;
+public:
+ explicit RuntimeContext(RuntimeRole r,LoopbackTransport* t=nullptr):role_(r),transport_(t){}
+ RuntimeRole Role()const{return role_;}
+ void Send(uint32_t channel,const std::vector<uint8_t>&payload){if(transport_)transport_->Send(channel,payload);}
+ LoopbackTransport*Transport()const{return transport_;}
+ ReplicationRuntime&Replication(){return replication_;}
+};
+}
