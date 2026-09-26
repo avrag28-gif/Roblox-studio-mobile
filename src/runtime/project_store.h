0bb6a01658eb2168a342e7ef3c3fec6ae0c707a8
avrag28-gif <avrag28@gmail.com>
2026-09-26 07:46:44 +0700

    feat: add project backup during runtime save

--- src/runtime/project_store.h
+++ src/runtime/project_store.h
@@ -8,6 +8,6 @@ struct ProjectStore {
  static constexpr int CurrentVersion=4;
  static std::string Escape(const std::string&s){std::string o;for(char c:s){if(c=='\\'||c=='"')o+='\\';o+=c;}return o;}
  static std::string Save(const DataModel& dm){std::ostringstream o;o<<"{\n  \"version\": "<<CurrentVersion<<",\n  \"instances\": [\n";auto xs=dm.GetDescendants();for(size_t i=0;i<xs.size();++i){auto*x=xs[i];o<<"    {\"name\":\""<<Escape(x->Name())<<"\",\"class\":\""<<Escape(x->ClassName())<<"\",\"parent\":\""<<(x->Parent()?Escape(x->Parent()->Name()):"")<<"\"}"<<(i+1<xs.size()?",":"")<<"\n";}o<<"  ]\n}\n";return o.str();}
- static bool Write(const DataModel&dm,const std::string&path){auto tmp=path+".tmp";std::ofstream f(tmp,std::ios::binary);if(!f)return false;auto s=Save(dm);f.write(s.data(),s.size());f.close();std::error_code ec;std::filesystem::rename(tmp,path,ec);if(ec){std::filesystem::remove(path,ec);std::filesystem::rename(tmp,path,ec);}return !ec;}
+ static bool Write(const DataModel&dm,const std::string&path){auto tmp=path+".tmp";auto bak=path+".bak";std::ofstream f(tmp,std::ios::binary);if(!f)return false;auto s=Save(dm);f.write(s.data(),s.size());f.flush();f.close();std::error_code ec;if(std::filesystem::exists(path,ec))std::filesystem::copy_file(path,bak,std::filesystem::copy_options::overwrite_existing,ec);std::filesystem::rename(tmp,path,ec);if(ec){std::filesystem::remove(path,ec);std::filesystem::rename(tmp,path,ec);}return !ec;}
 };
 }
