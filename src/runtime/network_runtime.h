d95174ff5d5afcf360c63601ab662ec50eed5b1f
avrag28-gif <avrag28@gmail.com>
2026-09-26 08:09:28 +0700

    fix: repair native engine compile blockers

--- src/runtime/network_runtime.h
+++ src/runtime/network_runtime.h
@@ -1,11 +1,17 @@
 #pragma once
-#include "tcp_transport.h"
+#include "../network/tcp_transport.h"
 #include "client_server.h"
 #include "../core/data_model.h"
 #include "../scripting/luau_service.h"
+#include <memory>
+#include <string>
+#include <vector>
 namespace rsm {
 class NetworkRuntime {
- TcpTransport transport_; RuntimeRole role_=RuntimeRole::Editor; std::unique_ptr<DataModel> game_; std::unique_ptr<LuauService> scripts_;
+ TcpTransport transport_;
+ RuntimeRole role_=RuntimeRole::Editor;
+ std::unique_ptr<DataModel> game_;
+ std::unique_ptr<LuauService> scripts_;
 public:
  explicit NetworkRuntime(RuntimeRole role=RuntimeRole::Editor):role_(role){}
  bool Start(DataModel*source){if(!source)return false;auto c=source->Clone();game_.reset(dynamic_cast<DataModel*>(c.release()));if(!game_)return false;scripts_=std::make_unique<LuauService>();scripts_->Bind(game_.get());return true;}
@@ -16,4 +22,5 @@ public:
  RuntimeRole Role()const{return role_;}
  DataModel*Game(){return game_.get();}
  void Stop(){transport_.Close();scripts_.reset();game_.reset();}
-}; }
+};
+}
