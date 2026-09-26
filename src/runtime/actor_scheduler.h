d088040b2bff3c98f4e482b607b08e6ba30c27b3
avrag28-gif <avrag28@gmail.com>
2026-09-26 08:28:55 +0700

    fix: allow actor scheduler pending query from const

--- src/runtime/actor_scheduler.h
+++ src/runtime/actor_scheduler.h
@@ -8,7 +8,7 @@
 #include <vector>
 namespace rsm {
 class ActorScheduler {
- std::mutex m_;std::condition_variable cv_;std::queue<std::function<void()>> q_;std::vector<std::thread> workers_;std::atomic<bool>stop_{false};
+ mutable std::mutex m_;std::condition_variable cv_;std::queue<std::function<void()>> q_;std::vector<std::thread> workers_;std::atomic<bool>stop_{false};
  void Worker(){for(;;){std::function<void()> fn;{std::unique_lock<std::mutex>l(m_);cv_.wait(l,[&]{return stop_||!q_.empty();});if(stop_&&q_.empty())return;fn=std::move(q_.front());q_.pop();}try{if(fn)fn();}catch(...){}}}
 public:
  explicit ActorScheduler(size_t n=1){for(size_t i=0;i<(n?n:1);++i)workers_.emplace_back([this]{Worker();});}
