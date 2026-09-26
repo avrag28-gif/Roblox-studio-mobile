a5125313d19698a34f253bdf3ae16f32bcd48d2a
avrag28-gif <avrag28@gmail.com>
2026-09-26 13:04:28 +0700

    Test polygon OBJ triangulation

--- tests/runtime_asset_security_test.cpp
+++ tests/runtime_asset_security_test.cpp
@@ -3,6 +3,7 @@
 #include "../src/runtime/actor_scheduler.h"
 #include "../src/security/project_validator.h"
 #include <cassert>
+#include "../src/assets/obj_importer.h"
 #include <atomic>
 #include <thread>
 #include <chrono>
@@ -11,5 +12,7 @@ int main(){
  std::string glb="glTF";glb.push_back(2);glb.push_back(0);glb.push_back(0);glb.push_back(0);glb.append(8,'\0');assert(!rsm::GltfImporter::ParseGlb(glb,m,e));
  rsm::AudioMixer mix(2);auto v=mix.AddVoice();assert(v<2);mix.SetVoice(v,1,0);float buf[8];mix.Mix(buf,4,2);assert(buf[0]>0);mix.Stop(v);
  rsm::ActorScheduler s(2);std::atomic<int>n{0};for(int i=0;i<10;i++)s.Enqueue([&]{++n;});for(int i=0;i<100&&n<10;i++)std::this_thread::sleep_for(std::chrono::milliseconds(1));assert(n==10);s.Shutdown();
- assert(rsm::ProjectValidator::SafeRelativePath("assets/a.obj"));assert(!rsm::ProjectValidator::SafeRelativePath("../secret"));return 0;
+ assert(rsm::ProjectValidator::SafeRelativePath("assets/a.obj"));assert(!rsm::ProjectValidator::SafeRelativePath("../secret"));
+  rsm::MeshData obj; std::string err; assert(rsm::ObjImporter::Parse("v 0 0 0\nv 1 0 0\nv 1 1 0\nv 0 1 0\nf 1 2 3 4\n",obj,err)); assert(obj.indices.size()==6);
+return 0;
 }
