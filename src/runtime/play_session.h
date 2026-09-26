6acde38789c944ebb96e4ead78dc3bafd46fcc11
avrag28-gif <avrag28@gmail.com>
2026-09-26 13:13:46 +0700

    Allow play session to start from DataModel

--- src/runtime/play_session.h
+++ src/runtime/play_session.h
@@ -4,8 +4,9 @@ namespace rsm {
 class PlaySession {
  Scene runtime_;bool running_=false;
 public:
- bool Start(const Scene&source){auto clone=source.Game().Clone();if(!clone)return false;runtime_.Reset();runtime_.Game().SetName(clone->Name());for(auto*x:clone->GetChildren()){auto c=x->Clone();if(c)Instance::SetParent(std::move(c),&runtime_.Game());}running_=true;return true;}
+ bool Start(const DataModel&source){auto clone=source.Game().Clone();if(!clone)return false;runtime_.Reset();runtime_.Game().SetName(clone->Name());for(auto*x:clone->GetChildren()){auto c=x->Clone();if(c)Instance::SetParent(std::move(c),&runtime_.Game());}running_=true;return true;}
  void Stop(){running_=false;runtime_.Reset();}
+ bool Start(const Scene&source){return Start(source.Game());}
  bool Running()const{return running_;}Scene&SceneData(){return runtime_;}
 };
 }
