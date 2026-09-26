faa400841308c04cf11b6efea89d760419c1604b
avrag28-gif <avrag28@gmail.com>
2026-09-26 07:46:36 +0700

    fix: clone editor scene into isolated play runtime

--- src/runtime/play_session.h
+++ src/runtime/play_session.h
@@ -1,3 +1,11 @@
 #pragma once
 #include "../scene/scene.h"
-namespace rsm { class PlaySession{Scene runtime_;bool running_=false;public:bool Start(const Scene& source){runtime_.Reset();(void)source;running_=true;return true;}void Stop(){running_=false;}bool Running()const{return running_;}Scene&SceneData(){return runtime_;}};}
+namespace rsm {
+class PlaySession {
+ Scene runtime_;bool running_=false;
+public:
+ bool Start(const Scene&source){auto clone=source.Game().Clone();if(!clone)return false;runtime_.Reset();runtime_.Game().SetName(clone->Name());for(auto*x:clone->GetChildren()){auto c=x->Clone();if(c)Instance::SetParent(std::move(c),&runtime_.Game());}running_=true;return true;}
+ void Stop(){running_=false;runtime_.Reset();}
+ bool Running()const{return running_;}Scene&SceneData(){return runtime_;}
+};
+}
